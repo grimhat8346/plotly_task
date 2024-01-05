4bd6e362df9cc0b43d8956a0f8877893be7eb560
grimhat8346 <grimhat@proton.me>
2024-01-05 12:11:15 -0800

    moved selectors to separate file

--- cypress/e2e/plotly.cy.js
+++ cypress/e2e/plotly.cy.js
@@ -1,3 +1,5 @@
+import selectors from '../support/selectors'
+
 describe('Cypress.io Website Tests', () => {
 
   beforeEach(() => {
@@ -6,20 +8,20 @@ describe('Cypress.io Website Tests', () => {
 
   it('should scroll to and verify weekly downloads number', () => {
     cy.contains('Loved by OSS, trusted by Enterprise').scrollIntoView();
-    cy.get('.font-bold.text-gray-1000.text-\\[28px\\].leading-\\[40px\\].font-primary').first()
+    cy.get(selectors.weeklyDownloadsText).first()
         .invoke ('text')
         .should('equal', '5M+')
   });
 
   it('should navigate to About Cypress from Company', () => {
-    cy.get('[id="dropdownCompany"]').trigger('mouseover');
-    cy.contains('About Cypress').click();
+    cy.get(selectors.dropdownCompany).trigger('mouseover');
+    cy.contains(selectors.aboutCypress).click();
     cy.url().should ('include', '/about-us')
   });
 
   it('should navigate to and verify npm install command', () => {
-    cy.get('[data-cy="header-install"]').click({force:true});
-    cy.contains('npm install cypress').click();
+    cy.get(selectors.installHeader).click({force:true});
+    cy.contains(selectors.npmInstallCypress).click();
     cy.window().then(win => {
       win.navigator.clipboard.readText().then(text => {
         expect(text).to.eq('npm install cypress --save-dev');
@@ -28,16 +30,16 @@ describe('Cypress.io Website Tests', () => {
   });
 
   it('should navigate to Visual Review from Product', () => {
-    cy.contains('Product').trigger('mouseover');
-    cy.contains('Visual Reviews').click();
+    cy.contains(selectors.productMenu).trigger('mouseover');
+    cy.contains(selectors.visualReviews).click();
     cy.url().should('include', 'visual_reviews');
   });
 
   it('should navigate to Test Analytics under Smart Orchestration and verify green circle', () => {
-    cy.contains('Product').trigger('mouseover');
-    cy.contains('Smart Orchestration').click();
+    cy.contains(selectors.productMenu).trigger('mouseover');
+    cy.contains(selectors.smartOrchestration).click();
     cy.scrollTo(0,8100);
-    cy.get('.border-jade-200').should('have.css', 'border-color', 'rgb(163, 231, 203)');
+    cy.get(selectors.greenCircle).should('have.css', 'border-color', 'rgb(163, 231, 203)');
   });
 
 });
---
+++ cypress/support/selectors.js
@@ -0,0 +1,14 @@
+const selectors = {
+    weeklyDownloadsText: '.font-bold.text-gray-1000.text-\\[28px\\].leading-\\[40px\\].font-primary',
+    dropdownCompany: '[id="dropdownCompany"]',
+    aboutCypress: 'About Cypress',
+    installHeader: '[data-cy="header-install"]',
+    npmInstallCypress: 'npm install cypress',
+    productMenu: 'Product',
+    visualReviews: 'Visual Reviews',
+    smartOrchestration: 'Smart Orchestration',
+    greenCircle: '.border-jade-200', 
+  };
+  
+  export default selectors;
+  
